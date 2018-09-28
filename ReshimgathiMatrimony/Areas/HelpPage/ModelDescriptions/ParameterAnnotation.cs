b495856609e992265136b362e155bc1764b6295d
Amit Patange <amitpatange88@gmail.com>
2018-09-28 19:26:35 +0530

    initia commit

---
+++ ReshimgathiMatrimony/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReshimgathiMatrimony.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ ReshimgathiMatrimony/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ReshimgathiMatrimony.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
